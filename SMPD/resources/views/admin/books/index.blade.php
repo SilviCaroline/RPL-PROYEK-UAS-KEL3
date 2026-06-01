77218331d1caf09b363b9643a2c4299d6462edcf
CellineOktavia <hartantocelline@gmail.com>
2026-06-01 17:55:57 +0700

    bro komit e

--- SMPD/resources/views/admin/books/index.blade.php
+++ SMPD/resources/views/admin/books/index.blade.php
@@ -105,7 +105,7 @@
                                 </th>
 
                                 <th class="p-4">
-                                    kode_buku
+                                    Kode Buku
                                 </th>
 
                                 <th class="p-4">
@@ -153,7 +153,7 @@
                                         <span
                                             class="font-mono text-sm bg-slate-100 px-2 py-1 rounded">
 
-                                            {{ $book->kode_buku }}
+                                            {{ $book->barcode }}
 
                                         </span>
 
