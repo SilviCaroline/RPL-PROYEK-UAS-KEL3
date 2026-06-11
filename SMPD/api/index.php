9d50bac4088352c263f1c4b58976f94c62d26bf1
Kevel Wijaya <kevelwijaya234@gmail.com>
2026-06-11 13:38:48 +0700

    Update index.php

--- SMPD/api/index.php
+++ SMPD/api/index.php
@@ -1,19 +1,27 @@
 <?php
 
+ob_start();
+
+register_shutdown_function(function () {
+    $error = error_get_last();
+
+    if ($error && in_array($error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
+        while (ob_get_level() > 0) {
+            ob_end_clean();
+        }
+        http_response_code(500);
+        header('Content-Type: application/json');
+        echo json_encode([
+            'fatal_error' => $error,
+            'php_version' => PHP_VERSION,
+        ]);
+    } else {
+        ob_end_flush();
+    }
+});
+
 $baseStorage = __DIR__ . '/../storage';
 $tmpStorage  = '/tmp/storage';
 
 if (!is_dir($tmpStorage)) {
     mkdir($tmpStorage, 0775, true);
-    foreach (['app', 'framework', 'logs'] as $dir) {
-        $src = "$baseStorage/$dir";
-        $dst = "$tmpStorage/$dir";
-        if (!is_dir($dst)) mkdir($dst, 0775, true);
-    }
-    foreach (['cache', 'cache/data', 'sessions', 'testing', 'views'] as $sub) {
-        $path = "$tmpStorage/framework/$sub";
-        if (!is_dir($path)) mkdir($path, 0775, true);
-    }
-}
-
-require __DIR__ . '/../public/index.php';
